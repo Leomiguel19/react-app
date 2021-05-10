fb3b149e2bd88c8d8123dd81e4aa6770cd4db0de
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-10 06:00:17 -0400

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+function HelloWorld(name, age){
+  let presentation = (
+    <div>
+      <h2>Hi, i'm {name}</h2>
+      <h2>i'm {age} years old</h2>
+    </div>
+  );
+  return presentation;
+}
+
 function App() {
+  let name = "Leonardo Guilarte";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there! Welcome to mi first project in react
         </p>
+          {HelloWorld(name, 22)}
         <a
           className="App-link"
           href="https://reactjs.org"
