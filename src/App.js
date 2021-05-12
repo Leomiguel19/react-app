b1f4640f86aace459aa7e3f9c29a979d039f35cb
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-12 08:11:49 -0400

    Creado componente Slider para la interfaz de la página

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './assets/css/App.css';
 import MiComponente from './components/MiComponente';
 import Peliculas from './components/Peliculas';
 import Header from './components/Header';
+import Slider from './components/Slider';
 
 function HelloWorld(name, age) {
   let presentation = (
@@ -22,6 +23,8 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Slider/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Slider.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class slider extends Component {
+  render() {
+    return (
+      <div id="slider" className="slider-big">
+        <h1>Bienvenido a mi primera app con React</h1>
+        <a href="#" className="btn-white">Ir al blog</a>
+      </div>
+    );
+  }
+}
+
+export default slider;
