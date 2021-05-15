1135f2f6192b0a0cb01d305cdab857bd6d3914e4
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-15 17:42:12 -0400

    Agregando la maquetacion del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,19 +3,58 @@ import Slider from './Slider';
 import Sidebar from './Sidebar';
 
 class Formulario extends Component {
+
+  nombreRef = React.createRef();
+  apellidosRef = React.createRef();
+  bioRef = React.createRef();
+  generoHombreRef = React.createRef();
+  generoMujerRef = React.createRef();
+  generoOtroeRef = React.createRef();
+  
+  
+  
+  recibirFormulario = (e) => {
+    e.preventDefault()
+    
+  }
+
   render() {
     return (
       <div id="formulario">
-        <Slider
-          title="Formulario"
-          size="slider-small"
-        />
         <div className="center">
           <div id="content">
-            {/*Listado de artículos que vendrán de la API rest de node*/}
+            <h1 className="subheader">Formulario</h1>
+            {/* Crear un formulario */}
+            <form className="mid-form" onSubmit={this.recibirFormulario}>
+              <div className="form-group">
+                <label htmlFor="nombre">Nombre</label>
+                <input type="text" name="nombre" ref={this.nombreRef} />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="apellidos">Apellidos</label>
+                <input type="text" name="apellidos" ref={this.apellidosRef} />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="bio">Biografia</label>
+                <textarea name="bio" ref={this.bioRef}></textarea>
+              </div>
+
+              <div className="form-group radibuttons">
+                <input type="radio" name="genero" value="hombre" ref={this.generoHombreRef}/> Hombre
+                <input type="radio" name="genero" value="mujer" ref={this.generoMujerRef}/> Mujer
+                <input type="radio" name="genero" value="otro" ref={this.generoOtroeRef}/> Otro
+              </div>
+
+              <div className="clearfix"></div>
+
+              <input type="submit" value="Enviar" className="btn btn-success" />
+
+            </form>
           </div>
-          
-          <Sidebar/>
+
+          <Sidebar />
 
         </div>
       </div>
