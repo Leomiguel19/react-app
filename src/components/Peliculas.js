79fa08978818068f052cb0bc66c5e610e998a795
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-15 11:29:42 -0400

    Metodos del ciclo de vida de React

--- src/components/Peliculas.js
+++ src/components/Peliculas.js
@@ -2,39 +2,50 @@ import React, { Component } from 'react';
 import Pelicula from './Pelicula';
 class Peliculas extends Component {
     state = {
-        peliculas: [
-            {
-                titulo: 'Demon Slayer: Mugen Train',
-                image: 'https://images-na.ssl-images-amazon.com/images/I/91qGYZ1BmOL._AC_SY679_.jpg'
-            },
-            {
-                titulo: 'The Silence of the Lambs',
-                image: 'https://upload.wikimedia.org/wikipedia/en/8/86/The_Silence_of_the_Lambs_poster.jpg'
-            },
-            {
-                titulo: 'The Karate Kid',
-                image: 'https://image.tmdb.org/t/p/w1280/x2zD90hccwLPSvcUgJ6KhwLW8KA.jpg'
-            },
-            {
-                titulo: 'La La Land',
-                image: 'https://premios.atrae.org/wp-content/uploads/2018/09/LA-LA-LAND.jpg'
-            },
-            {
-                titulo: 'Avengers: EndGame',
-                image: 'https://lumiere-a.akamaihd.net/v1/images/690x0w_f1b0509a.jpeg?region=0%2C0%2C690%2C1035'
-            },
-            {
-                titulo: 'Captain America: Civil War',
-                image: 'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/b/bf/Captain_America_Civil_War_-_Poster_definitivo.png/revision/latest?cb=20191029195149&path-prefix=es'
-            },
-        ],
-        name: 'Leonardo Guilarte',
-        favorita: {}
+       
     }
 
     favorita = (peli) => {
         this.setState({
             favorita: peli
+        })
+    }
+
+    componentDidMount(){
+        //alert("Ya se monto el componente")
+    }
+    
+    componentWillMount(){
+        //alert("Se va a montar el componente")
+        this.setState({
+            peliculas: [
+                {
+                    titulo: 'Demon Slayer: Mugen Train',
+                    image: 'https://images-na.ssl-images-amazon.com/images/I/91qGYZ1BmOL._AC_SY679_.jpg'
+                },
+                {
+                    titulo: 'The Silence of the Lambs',
+                    image: 'https://upload.wikimedia.org/wikipedia/en/8/86/The_Silence_of_the_Lambs_poster.jpg'
+                },
+                {
+                    titulo: 'The Karate Kid',
+                    image: 'https://image.tmdb.org/t/p/w1280/x2zD90hccwLPSvcUgJ6KhwLW8KA.jpg'
+                },
+                {
+                    titulo: 'La La Land',
+                    image: 'https://premios.atrae.org/wp-content/uploads/2018/09/LA-LA-LAND.jpg'
+                },
+                {
+                    titulo: 'Avengers: EndGame',
+                    image: 'https://lumiere-a.akamaihd.net/v1/images/690x0w_f1b0509a.jpeg?region=0%2C0%2C690%2C1035'
+                },
+                {
+                    titulo: 'Captain America: Civil War',
+                    image: 'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/b/bf/Captain_America_Civil_War_-_Poster_definitivo.png/revision/latest?cb=20191029195149&path-prefix=es'
+                },
+            ],
+            name: 'Leonardo Guilarte',
+            favorita: {}
         })
     }
 
