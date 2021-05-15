3255f1b9788883a983da520f6974da72496d2650
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-15 12:47:44 -0400

    Haciendo funcional el menú de navegación

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import './assets/css/App.css';
 
-// Importar componentes
-import Header from './components/Header';
-import Slider from './components/Slider';
-import Sidebar from './components/Sidebar';
-import Footer from './components/Footer';
-//import Peliculas from './components/Peliculas';
 import Router from './Router';
 
 function App() {
-  let buttonString = "Ver más";
 
   return (
     <div className="App">
-      <Header />
-      <Slider 
-        title="Bienvenido a mi primera app con React" 
-        btn={buttonString} 
-      />
-
-      <div className="center">
-
         <Router/>
-        {/* <Peliculas /> */}
-        <Sidebar />
-
-        <div className="clearfix"></div>
-      </div>
-      <Footer />
     </div>
   );
 }
--- src/Router.js
+++ src/Router.js
@@ -5,52 +5,72 @@ import Peliculas from './components/Peliculas';
 import SeccionPruebas from './components/SeccionPruebas';
 import MiComponente from './components/MiComponente';
 import Error from './components/Error';
+// Importar componentes
+import Header from './components/Header';
+import Slider from './components/Slider';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+//import Peliculas from './components/Peliculas';
 
 class Router extends Component {
   render() {
+    let buttonString = "Ver más";
     return (
       <BrowserRouter>
 
-        {/* CONFIGURAR RUTAS Y PÁGINAS */}
-        <Switch>
-          <Route exact path="/" component={Peliculas} />
-          <Route exact path="/segunda-ruta" component={MiComponente} />
-          <Route exact path="/ruta-prueba" component={SeccionPruebas} />
+        <Header />
+        <Slider
+          title="Bienvenido a mi primera app con React"
+          btn={buttonString}
+        />
+        <div className="center">
 
-          <Route exact path="/pagina-1" render={() => (
-            <React.Fragment>
-              <h1>Hello world from route: PAGINA 1</h1>
-              <MiComponente saludo="Hello my friend" />
-            </React.Fragment>
-          )} />
+          {/* CONFIGURAR RUTAS Y PÁGINAS */}
+          <Switch>
+            <Route exact path="/" component={Peliculas} />
+            <Route exact path="/segunda-ruta" component={MiComponente} />
+            <Route exact path="/ruta-prueba" component={SeccionPruebas} />
 
-          <Route exact path="/pruebas/:nombre/:apellido?" render={(props) => {
-            let nombre = props.match.params.nombre;
-            let apellido = props.match.params.apellido;
+            <Route exact path="/pagina-1" render={() => (
+              <React.Fragment>
+                <h1>Hello world from route: PAGINA 1</h1>
+                <MiComponente saludo="Hello my friend" />
+              </React.Fragment>
+            )} />
 
-            return (
-              <div id="content">
-                <h1 className="subheader">Página de pruebas</h1>
-                <h2>
-                  {nombre && !apellido &&
-                    <div>
-                      {nombre}
-                    </div>
-                  }
-                  {nombre && apellido &&
-                    <div>
-                      {nombre} {apellido}
-                    </div>
-                  }
-                </h2>
-              </div>
-            );
-          }
-          } />
+            <Route exact path="/pruebas/:nombre/:apellido?" render={(props) => {
+              let nombre = props.match.params.nombre;
+              let apellido = props.match.params.apellido;
 
-          <Route component={Error} />
-        </Switch>
+              return (
+                <div id="content">
+                  <h1 className="subheader">Página de pruebas</h1>
+                  <h2>
+                    {nombre && !apellido &&
+                      <div>
+                        {nombre}
+                      </div>
+                    }
+                    {nombre && apellido &&
+                      <div>
+                        {nombre} {apellido}
+                      </div>
+                    }
+                  </h2>
+                </div>
+              );
+            }
+            } />
 
+            <Route component={Error} />
+          </Switch>
+
+
+          <Sidebar />
+
+          <div className="clearfix"></div>
+        </div>
+        <Footer />
       </BrowserRouter>
     );
   }
