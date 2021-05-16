0d4e1f51f61cc37fa1f9e8eb77994a1530a09b99
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-15 22:43:09 -0400

    Mostrar el listado de articulos

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-class Articles extends Component{
+class Articles extends Component {
 
   state = {
     articles: [],
     status: null
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getArticles();
   }
 
@@ -24,28 +24,47 @@ class Articles extends Component{
       })
   }
 
-  render(){
-    if(this.state.articles.length >= 1){
-      return(
+  render() {
+    if (this.state.articles.length >= 1) {
+
+      var listArticles = this.state.articles.map((article) => {
+        return (
+          <article className="article-item" id="article-template">
+            <div className="image-wrap">
+              <img src="https://unhabitatmejor.leroymerlin.es/sites/default/files/styles/header_category/public/2018-10/4%20paisaje%20macedonia.jpg?itok=AELknmF8" alt="Paisaje" />
+            </div>
+
+            <h2>{article.title}</h2>
+            <span className="date">
+              {article.date}
+            </span>
+            <a href="#">Leer más</a>
+
+            <div className="clearfix"></div>
+          </article>
+        )
+      })
+
+      return (
         <div id="articles">
-          <h1>LISTADO DE ARTÍCULOS</h1>
+          {listArticles}
         </div>
-      );  
-    }else if(this.state.articles.length === 0 && this.state.articles.status === 'success'){
-      return(
+      );
+    } else if (this.state.articles.length === 0 && this.state.articles.status === 'success') {
+      return (
         <div id="articles">
           <h2 className="subheader">No hay artículos para mostrar</h2>
           <p>Todavia no hay contenido en esta sección</p>
         </div>
-      );  
-    }else{
-      return(
+      );
+    } else {
+      return (
         <div id="articles">
           <h2 className="subheader">Cargando...</h2>
           <p>Espere mientras carga el contenido...</p>
         </div>
       );
-  
+
     }
   }
 }
